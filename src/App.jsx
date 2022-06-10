439fb53287afd0888af2dd7627e1801f1aa59674
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-06-09 19:03:46 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { React, useState, useEffect } from 'react'
+import { FaTwitterSquare } from 'react-icons/fa'
+import { m } from 'framer-motion'
+import './index.css'
+
+function App() {
+    const [quote, setQuote] = useState({
+        text: '',
+        author: '',
+    })
+    const [allQuotes, setAllQuotes] = useState([])
+    useEffect(() => {
+        fetch('https://type.fit/api/quotes')
+            .then((res) => res.json())
+            .then((data) => {
+                getQuote(data)
+                setAllQuotes(data)
+            })
+    }, [])
+
+    function getQuote(data) {
+        const randNumber = Math.floor(Math.random() * data.length)
+        const randAuthor = data[randNumber].author
+        const randText = data[randNumber].text
+
+        setQuote((prevQuote) => ({
+            ...prevQuote,
+            text: randText,
+            author: randAuthor,
+        }))
+    }
+    return (
+        <div onLoad={getQuote} id="wrapper">
+            <m.div animate={{ x: 100 }} id="quote-box">
+                <p id="text">"{quote.text}"</p>
+                <p id="author">{quote.author}</p>
+                <div id="button-cont">
+                    <a
+                        id="tweet-quote"
+                        href={`https://twitter.com/intent/tweet?text=${
+                            '"' + quote.text + '"' + ' ' + quote.author
+                        }`}
+                        taget="_blank"
+                    >
+                        <button id="but-twitter">
+                            <FaTwitterSquare size={25} />
+                        </button>
+                    </a>
+                    <button onClick={() => getQuote(allQuotes)} id="new-quote">
+                        New Quote
+                    </button>
+                </div>
+            </m.div>
+        </div>
+    )
+}
+
+export default App
